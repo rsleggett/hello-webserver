814512fc9e8dbebdfa5bbed13bf757efde785858
rsleggett <rsleggett@googlemail.com>
2024-11-03 21:34:30 +0100

    Add option to use rayon as thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 use std::{
-    fs,
-    io::{prelude::*, BufReader },
-    net::{ TcpListener, TcpStream }, thread, time::Duration
+    env, fs, io::{prelude::*, BufReader }, net::{ TcpListener, TcpStream }, thread, time::Duration
 };
+use rayon::ThreadPoolBuilder;
 use hello_webserver::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let use_rayon = env::var("USE_RAYON").is_ok();
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    match use_rayon {
+        true => run_rayon_server(listener),
+        false => run_custom_server(listener)
+    };
+}
+
+fn run_custom_server(listener: TcpListener) {
     let pool = ThreadPool::build(4).unwrap_or_else(|_e| { panic!("PoolCreationError"); });
-    
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
         
         pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn run_rayon_server(listener: TcpListener) {
+    let pool = ThreadPoolBuilder::new().num_threads(4).build().unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        pool.spawn(|| {
             handle_connection(stream);
         });
     }
